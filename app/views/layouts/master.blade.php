7b74332e38ee6c43de5e78eb421aef1c765fa3d6
zgulde <zachgulde@gmail.com>
2016-01-19 10:22:34 -0600

    added a master layout

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+    @yield('top-script')
+</head>
+<body>
+    @yield('content')
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+    @yield('bottom-script')
+</body>
+</html>
