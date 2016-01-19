c2ea303c602e944de251b9acae7d35421e707830
zgulde <zachgulde@gmail.com>
2016-01-19 13:56:20 -0600

    started work on the landing page
    the '/' route now returns the main view
    basic hero area for the landing page
    basic css styling

---
+++ app/views/main.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.master')
+
+@section('title', 'happttails!')
+
+@section('content')
+    <div class="hero-title light-blue">
+        <h1 class="center">happy tails!</h1>
+    </div>
+    <div class="hero-body">
+        <div class="row">
+            <div class="col m6">
+                <div class="card-panel indigo darken-4 white-text hoverable">
+                    <h1 class="center">Post a pet</h1>
+                </div>
+            </div>
+            <div class="col m6">
+                <div class="card-panel indigo darken-4 white-text hoverable">
+                    <h1 class="center">Find a pet</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="auth-bar blue">
+        login...etc
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="input-field col m3"><select name="" id="">
+                    <option value="" disabled="" selected=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                </select></div>
+            <div class="input-field col m3"><select name="" id="">
+                    <option value="" disabled="" selected=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                </select></div>
+            <div class="input-field col m3"><select name="" id="">
+                    <option value="" disabled="" selected=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                </select></div>
+            <div class="input-field col m3"><select name="" id="">
+                    <option value="" disabled="" selected=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                </select></div>
+        </div>
+    </div>
+@stop
