dad41f7467d03d3eb8f10ad4a13e91b00814ca65
zgulde <zachgulde@gmail.com>
2016-01-20 08:48:39 -0600

    added a third button to the landing page and changed the wording on the main buttons

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -8,47 +8,24 @@
     </div>
     <div class="hero-body">
         <div class="row">
-            <div class="col m6">
+            <div class="col m4">
                 <div class="card-panel indigo darken-4 white-text hoverable">
-                    <h1 class="center">Post a pet</h1>
+                    <h1 class="center">View found/ adoptable</h1>
                 </div>
             </div>
-            <div class="col m6">
+            <div class="col m4">
                 <div class="card-panel indigo darken-4 white-text hoverable">
-                    <h1 class="center">Find a pet</h1>
+                    <h1 class="center">New Post</h1>
+                </div>
+            </div>
+            <div class="col m4">
+                <div class="card-panel indigo darken-4 white-text hoverable">
+                    <h1 class="center">view lost</h1>
                 </div>
             </div>
         </div>
     </div>
     <div class="auth-bar blue">
         login...etc
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="input-field col m3"><select name="" id="">
-                    <option value="" disabled="" selected=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                </select></div>
-            <div class="input-field col m3"><select name="" id="">
-                    <option value="" disabled="" selected=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                </select></div>
-            <div class="input-field col m3"><select name="" id="">
-                    <option value="" disabled="" selected=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                </select></div>
-            <div class="input-field col m3"><select name="" id="">
-                    <option value="" disabled="" selected=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                </select></div>
-        </div>
     </div>
 @stop
