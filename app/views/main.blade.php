d1727a9c67226dd329aeef69f0aa5888c024109a
Sj <sjmrstevens@gmail.com>
2016-01-21 14:56:07 -0600

    Changed the main hero welcome to a slider set of images

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -3,9 +3,38 @@
 @section('title', 'happytails!')
 
 @section('content')
-    <div class="hero-title light-blue">
-        <h2 class="center">happy tails!</h1>
-    </div>
+  <div class="slider">
+    <ul class="slides">
+      <li>
+        <img src="http://lorempixel.com/580/250/nature/1"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="http://lorempixel.com/580/250/nature/2"> <!-- random image -->
+        <div class="caption left-align">
+          <h3>Left Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="http://lorempixel.com/580/250/nature/3"> <!-- random image -->
+        <div class="caption right-align">
+          <h3>Right Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="http://lorempixel.com/580/250/nature/4"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+    </ul>
+  </div>
     <div class="hero-body">
         <div class="row">
             <div class="col m4">
@@ -25,8 +54,12 @@
             </div>
         </div>
     </div>
-    <div class="auth-bar blue">
-        login...etc
+    <div class="valign-wrapper auth-bar blue">
+        <div class="row">
+            <div class="col s6 offset-s5">
+                <a class="valign waves-effect waves-light btn-large">Login</a>
+            </div>
+        </div>
     </div>
     <!--Start Animals-->
     <div class="row">
@@ -67,3 +100,10 @@
     <!--End Animals-->
 
 @stop
+@section('bottom-script')
+<script type="text/jquery">
+    $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+
+</script>
