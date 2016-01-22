99679b7e0be4907defce28576be07506990e4347
zgulde <zachgulde@gmail.com>
2016-01-22 12:14:31 -0600

    moved the styleFixes js from the master layout to just the landing page

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -13,6 +13,7 @@
         $('select').material_select();
     });
 </script>
+<script src="/js/styleFixes.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
 <script src="/js/app.js"></script>
 @stop
@@ -56,8 +57,9 @@
     </div>
 
     <!--Start Animals-->
+    <div class="container">
     <div class="row">
-        <div class="col s4" ng-repeat="pet in pets | filter:search">
+        <div class="col m4" ng-repeat="pet in pets | filter:search">
             <div class="card hoverable" ng-click="openPetModal(pet.id)">
                 <div class="card-image ">
                     <img src="http://placehold.it/400">
@@ -69,15 +71,16 @@
             </div>
         </div>
     </div>
+    </div>
     <!--End Animals-->
 
     {{-- individual animal modal --}}
     <div class="modal" id="show-pet-modal">
-        <h4><% ctrl.displayedPet.name %></h4>
+        <h4><% displayedPet.name %></h4>
         <div class="col s3" ng-repeat="path in displayedPet.images">
             <img ng-src="<% path %>">
         </div>
-        <p><% ctrl.displayedPet.description %></p>
+        <p><% displayedPet.description %></p>
         <p>posted by <a href="/users/<% displayedPet.user.id %>"><% displayedPet.user.email %></a></p>
     </div>
 
