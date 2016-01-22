2a51593cb130b8f2fa3fdec829431f8910577c20
Jonathan R <jreyes.satx@gmail.com>
2016-01-22 14:45:56 -0600

    Pets controller call store, update, and destroy Pet model methods.

--- app/controllers/PetsController.php
+++ app/controllers/PetsController.php
@@ -14,25 +14,13 @@ class PetsController extends \BaseController {
 					->join('breeds', 'breeds.id', '=', 'pets.breed_id')
 					->leftJoin('shelters', 'shelters.id', '=', 'pets.shelter_id')
 					->leftJoin('users', 'users.id', '=', 'pets.user_id');
-		
-		//If search will be added later, use if with where/orWhere clauses and 'like' and '%' before selects
 
-		$query->select('pets.id', 'pets.species', 'pets.status', 'pets.color', 'pets.age', 'pets.description', 'pets.gender', 'breeds.breed', 'shelters.id', 'shelters.name', 'shelters.url', 'shelters.img_path', 'users.id', 'users.email', 'users.first_name', 'users.last_name');
-		
+        $array = [];
+        $array['pets'] = $query->select('pets.id as pet_id', 'pets.species', 'pets.status', 'pets.color', 'pets.age', 'pets.description', 'pets.gender', 'breeds.breed')->get();
+        $array['shelters'] = $query->select('shelters.id as shelter_id', 'shelters.name as shelter_name', 'shelters.url', 'shelters.img_path')->get();
+        $array['users'] = $query->select('users.id as user_id', 'users.email as user_email', 'users.first_name as user_first', 'users.last_name as user_last')->get();
 
-		$pets = $query->get();
-
-		return Response::json($pets);
-	}
-
-	/**
-	 * Show the form for creating a new pet
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return View::make('pets.create');
+		return Response::json($array);
 	}
 
 	/**
@@ -49,37 +37,9 @@ class PetsController extends \BaseController {
 			return Redirect::back()->withErrors($validator)->withInput();
 		};
 
+		$result = Pet::create($data);
 
-
-		Pet::create($data);
-
-		return Redirect::route('pets.index');
-	}
-
-	/**
-	 * Display the specified pet.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		$pet = Pet::findOrFail($id);
-
-		return View::make('pets.show', compact('pet'));
-	}
-
-	/**
-	 * Show the form for editing the specified pet.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		$pet = Pet::find($id);
-
-		return View::make('pets.edit', compact('pet'));
+		return $result;
 	}
 
 	/**
@@ -99,9 +59,9 @@ class PetsController extends \BaseController {
 			return Redirect::back()->withErrors($validator)->withInput();
 		}
 
-		$pet->update($data);
+		$result = $pet->update($data);
 
-		return Redirect::route('pets.index');
+		return $result;
 	}
 
 	/**
@@ -112,9 +72,9 @@ class PetsController extends \BaseController {
 	 */
 	public function destroy($id)
 	{
-		Pet::destroy($id);
+		$result = Pet::destroy($id);
 
-		return Redirect::route('pets.index');
+		return $result;
 	}
 
 }
