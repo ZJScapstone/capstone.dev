d3922ee7f8bb96d7f3d0c27947d19a016caa5aa6
zgulde <zachgulde@gmail.com>
2016-01-22 12:18:08 -0600

    whitespace

--- public/js/styleFixes.js
+++ public/js/styleFixes.js
@@ -17,14 +17,14 @@ $(document).ready(function(){
 
     $(window).resize(function(){
         $('body').css('padding-bottom', (navbarHeight * 2) + 'px');
-            $('.modal-trigger').click(function(){
-                $modal = $($(this).attr('href'));
-                setTimeout( function(){
-                    $modal.css({
-                        "transition": "all 0.3s",
-                        "top": "5%"
-                    });
-                }, 400);
-            });
+        $('.modal-trigger').click(function(){
+            $modal = $($(this).attr('href'));
+            setTimeout( function(){
+                $modal.css({
+                    "transition": "all 0.3s",
+                    "top": "5%"
+                });
+            }, 400);
+        });
     });
 });
