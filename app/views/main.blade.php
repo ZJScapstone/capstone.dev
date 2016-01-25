4e7c5440f4c61cb001f7cf64a418010d402a06d1
Jonathan R <jreyes.satx@gmail.com>
2016-01-25 15:25:34 -0600

    updated footer, master layout, main index

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -4,7 +4,7 @@
 
 @section('bottom-script')
 <script>
-    $(document).ready(function(){
+    $(document).ready(function() {
         // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
         $('.modal-trigger').leanModal();
     });
@@ -12,6 +12,14 @@
     $(document).ready(function() {
         $('select').material_select();
     });
+
+    $(document).ready(function() {
+       var options = [
+           {selector: '#test_toggle', offset: 300, callback: 'Materialize.fadeInImage("#test_toggle")'}
+       ];
+        Materialize.scrollFire(options);
+    });
+
 </script>
 <script src="/js/styleFixes.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
@@ -20,44 +28,191 @@
 
 @section('content')
 <div ng-app="petsApp" ng-controller="PetsController as ctrl">
-    <div class="hero-title light-blue">
-        <h2 class="center">happy tails!</h1>
+    <div class="hero-title deep-orange darken-3 z-depth-1">
+        <h1 class="center white-text">happy tails!</h1>
     </div>
+        <div class="row z-depth-1">
+            <div class="col s12">
+                <ul class="tabs">
+                    <li class="tab col s3"><a href="#">Pets</a></li>
+                    <li class="tab col s3"><a href="#">Docs</a></li>
+                    <li class="tab col s3"><a href="#">Forum</a></li>
+                    <li class="tab col s3"><a href="#">Events</a></li>
+                </ul>
+            </div>
+        </div>
     <div class="hero-body">
-        <div class="row">
-            <div class="col m4">
-                <div class="card-panel indigo darken-4 white-text hoverable">
-                    <h1 class="center">View found/ adoptable</h1>
-                </div>
-            </div>
-            <div class="col m4">
-                <a href="#pets-create-modal" class="modal-trigger">
-                    <div class="card-panel indigo darken-4 white-text hoverable">
-                        <h1 class="center">New Post/ Second/ Third</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col md4">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img src="http://placehold.it/275">
+                            <span class="card-title">Fido</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Ace Ventura Pet Description</p>
+                        </div>
                     </div>
-                </a>
-            </div>
-            <div class="col m4">
-                <div class="card-panel indigo darken-4 white-text hoverable">
-                    <h1 class="center">view lost/ Second/ Third</h1>
+                </div>
+                <div class="col md4">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img src="http://placehold.it/275">
+                            <span class="card-title">Meowers</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Ace Ventura Pet Description</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col md4">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img src="http://placehold.it/275">
+                            <span class="card-title">Adopt Me</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Ace Ventura Pet Description</p>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col md4">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img src="http://placehold.it/275">
+                            <span class="card-title">Fido</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Ace Ventura Pet Description</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col md4">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img src="http://placehold.it/275">
+                            <span class="card-title">Meowers</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Ace Ventura Pet Description</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col md4">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img src="http://placehold.it/275">
+                            <span class="card-title">Adopt Me</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Ace Ventura Pet Description</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col md4">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img src="http://placehold.it/275">
+                            <span class="card-title">Fido</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Ace Ventura Pet Description</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col md4">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img src="http://placehold.it/275">
+                            <span class="card-title">Meowers</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Ace Ventura Pet Description</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col md4">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img src="http://placehold.it/275">
+                            <span class="card-title">Adopt Me</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Ace Ventura Pet Description</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- filters --}}
+            <div class="container filters">
+                <input type="text" ng-model="search">
+            </div>
+        </div>
+        <div class="fixed-action-btn" id="test_toggle">
+            <a class="btn-floating btn-large deep-orange darken-3">
+                <i class="material-icons">shopping_basket</i>
+            </a>
+            <ul>
+                <li>
+                    <a class="btn-floating red modal-trigger" href="#pets-create-modal">
+                        <i class="material-icons">mode_edit</i>
+                    </a>
+                </li>
+                <li>
+                    <a class="btn-floating green" href="#">
+                        <i class="material-icons">loyalty</i>
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
 
-    {{-- pets create form modal --}}
 
+    {{-- pets create form modal --}}
     <div id="pets-create-modal" class="modal">
         @include('pets.create')
     </div>
 
-    {{-- filters --}}
-    <div class="container filters">
-        <input type="text" ng-model="search">
+    {{-- individual animal modal --}}
+    <div class="modal" id="show-pet-modal">
+        <h4><% displayedPet.name %></h4>
+        <div class="col s3" ng-repeat="path in displayedPet.images">
+            <img ng-src="<% path %>">
+        </div>
+        <p><% displayedPet.description %></p>
+        <p>posted by <a href="/users/<% displayedPet.user.id %>"><% displayedPet.user.email %></a></p>
     </div>
+</div>
+        {{--<div class="row">--}}
+            {{--<div class="col m4">--}}
+                {{--<div class="card-panel indigo darken-4 white-text hoverable">--}}
+                    {{--<h1 class="center">View found/ adoptable</h1>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="col m4">--}}
+                {{--<a href="#pets-create-modal" class="modal-trigger">--}}
+                    {{--<div class="card-panel indigo darken-4 white-text hoverable">--}}
+                        {{--<h1 class="center">New Post/ Second/ Third</h1>--}}
+                    {{--</div>--}}
+                {{--</a>--}}
+            {{--</div>--}}
+            {{--<div class="col m4">--}}
+                {{--<div class="card-panel indigo darken-4 white-text hoverable">--}}
+                    {{--<h1 class="center">view lost/ Second/ Third</h1>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+
+
+
 
     <!--Start Animals-->
-    <div class="container">
+{{--    <div class="container">
     <div class="row">
         <div class="col m4" ng-repeat="pet in pets | filter:search">
             <div class="card hoverable" ng-click="openPetModal(pet.id)">
@@ -71,18 +226,6 @@
             </div>
         </div>
     </div>
-    </div>
+    </div>--}}
     <!--End Animals-->
-
-    {{-- individual animal modal --}}
-    <div class="modal" id="show-pet-modal">
-        <h4><% displayedPet.name %></h4>
-        <div class="col s3" ng-repeat="path in displayedPet.images">
-            <img ng-src="<% path %>">
-        </div>
-        <p><% displayedPet.description %></p>
-        <p>posted by <a href="/users/<% displayedPet.user.id %>"><% displayedPet.user.email %></a></p>
-    </div>
-
-</div>
 @stop
