1f17b75ed44df5e10a5472b1bb6bbb040df09ca5
zgulde <zachgulde@gmail.com>
2016-01-26 13:59:25 -0600

    wrapped every pet in a div to prevent row collision

--- app/views/pets/index.blade.php
+++ app/views/pets/index.blade.php
@@ -1,13 +1,15 @@
 <div class="container">
     <div class="row">
-        <div class="col m4" ng-repeat="pet in pets | filter:search">
-            <div class="card hoverable" ng-click="openPetModal(pet.id)">
-                <div class="card-image ">
-                    <img ng-src="<% pet.images[0].img_path %>">
-                    <span class="card-title"><% pet.name %></span>
-                </div>
-                <div class="card-content">
-                    <p><% pet.description %></p>
+        <div class="pet" ng-repeat="pet in pets | filter:search">
+            <div class="col m4">
+                <div class="card hoverable" ng-click="openPetModal(pet.id)">
+                    <div class="card-image ">
+                        <img ng-src="<% pet.images[0].img_path %>">
+                        <span class="card-title"><% pet.name %></span>
+                    </div>
+                    <div class="card-content">
+                        <p><%$index + pet.description | limitTo : 50 : 0 %>...</p>
+                    </div>
                 </div>
             </div>
         </div>
