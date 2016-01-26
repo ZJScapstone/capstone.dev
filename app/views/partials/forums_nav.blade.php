f2989920f5e96136f5ca01482c7477bee8bdaf46
Jonathan R <jreyes.satx@gmail.com>
2016-01-26 11:47:45 -0600

    side_nav partials updates for responsiveness

--- app/views/partials/forums_nav.blade.php
+++ app/views/partials/forums_nav.blade.php
@@ -1,7 +1,10 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sunrey
- * Date: 1/26/16
- * Time: 00:01
- */
+@include('partials.side_nav')
+        <li class="bold"><a href="#" class="waves-effect waves-orange">New Fosters</a></li>
+        <li class="bold"><a href="#" class="waves-effect waves-orange">Medical Care</a></li>
+        <li class="bold"><a href="#" class="waves-effect waves-orange">Behavior</a></li>
+        <li class="bold"><a href="#" class="waves-effect waves-orange">Diet</a></li>
+        <li class="bold"><a href="#" class="waves-effect waves-orange">Other Resources</a></li>
+        <li><img src="/img/sapa_logo.png" alt="San Antonio Pets Alive" class="responsive-img"></li>
+    </ul>
+    <a href="#" data-activates="nav-mobile" class="button-collapse top-nav waves-effect waves-light circle hide-on-large-only"><i class="mdi-navigation-menu"></i></a>
+</div>
--- public/js/side_nav.js
+++ public/js/side_nav.js
@@ -1,3 +1,30 @@
-/**
- * Created by sunrey on 1/26/16.
- */
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+    $(".collapsible").collapsible();
+    $(".button-collapse").sideNav('show');
+    $(".button-collapse").sideNav('hide');
+
+    // icon click
+    $('ul#nav-mobile li.search .search-wrapper i.material-icons').click(function() {
+        if ($('.search-results .focused').length) {
+            $('.search-results .focused').first()[0].click();
+        } else if ($('.search-results').children().length) {
+            $('.search-results').children().first()[0].click();
+        }
+    });
+
+    $('input#search').focus(function() {
+        $(this).parent().addClass('focused');
+    });
+
+    $('input#search').blur(function() {
+        if (!$(this).val()) {
+            $(this).parent().removeClass('focused');
+        }
+    });
+
+    //$('.datepicker').pickadate({
+    //    selectMonths: true,
+    //    selectYears: 5
+    //});
+});
