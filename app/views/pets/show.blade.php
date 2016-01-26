036afc760a8c73f13bb5983c590a26bbbc3ff818
zgulde <zachgulde@gmail.com>
2016-01-26 12:33:48 -0600

    added pet info the the show modal

--- app/views/pets/show.blade.php
+++ app/views/pets/show.blade.php
@@ -1,6 +1,25 @@
-<h4><% displayedPet.name %></h4>
-<div class="col s3" ng-repeat="image in displayedPet.images">
-    <img ng-src="<% image.img_path %>">
+<h2 class="center"><% displayedPet.name %></h2>
+<div class="row">
+    <div class="col m6">
+        <p>Status: <% displayedPet.status %></p>
+        <p>posted by <a href="/users/<% displayedPet.user_id %>"><% displayedPet.user %></a> <% displayedPet.posted %></p>
+        <p>ID#: <% displayedPet.a_num %></p>
+    </div>
+    <div class="col m6">
+        <p>Species: <% displayedPet.species %></p>
+        <p>Gender: <% displayedPet.gender %></p>
+        <p>Breed: <% displayedPet.breed %></p>
+    </div>
 </div>
-<p><% displayedPet.description %></p>
-<p>posted by <a href="/users/<% displayedPet.user.id %>"><% displayedPet.user.email %></a></p>
+<div class="row">
+    <div class="col m12">
+        <p><% displayedPet.description %></p>
+    </div>
+</div>
+<div class="col s3" ng-repeat="image in displayedPet.images">
+    <div class="card hoverable">
+        <div class="card-image">
+            <img ng-src="<% image.img_path %>">
+        </div>
+    </div>
+</div>
