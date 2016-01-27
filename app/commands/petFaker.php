89d9e753e14e7532e2edc3bdc492179144b4d3b7
zgulde <zachgulde@gmail.com>
2016-01-26 22:27:33 -0600

    updated the petFaker command

--- app/commands/petFaker.php
+++ app/commands/petFaker.php
@@ -38,10 +38,14 @@ class petFaker extends Command {
 	public function fire()
 	{
 
-		echo "Lets create some pet postings!\n";
-		echo "This will create a pet record with the prompted information,\n";
-		echo "a random species, status, age, and color,\n";
-		echo "and the first breed, shelter, and user\n\n";
+		echo "\n--- Lets create some pet postings! ---\n\n";
+		echo "| This will create a new pet record with the prompted information,\n";
+		echo "| a random status, age, and color, and user, species, and breed\n";
+		echo "| ids that belong to the first record of each in the database.\n\n";
+
+		echo "| This command will also get a list of the files in /img/uploads\n";
+		echo "| and select one at random from the list to create a new image\n";
+		echo "| object and record to tie to the pet object we just created.\n\n";
 
 		echo "name \n> ";
 		$name = $this->ask('');
@@ -70,17 +74,25 @@ class petFaker extends Command {
 		$pet = new Pet();
 
 		$pet->name        = $name;
-        $pet->species     = $species;
-        $pet->status      = $status;
-        $pet->color       = $color;
-        $pet->age         = $age;
-        $pet->description = $description;
-        $pet->gender      = $gender;
-        $pet->a_num       = $a_num;
-        $pet->breed_id    = Breed::first()->id;
-        $pet->user_id     = User::first()->id;
+		$pet->species_id  = Species::first()->id;
+		$pet->status      = $status;
+		$pet->color       = $color;
+		$pet->age         = $age;
+		$pet->description = $description;
+		$pet->gender      = $gender;
+		$pet->a_num       = $a_num;
+		$pet->breed_id    = Breed::first()->id;
+		$pet->user_id     = User::first()->id;
 
         $pet->save();
+
+		$images = explode("\n", trim(`ls public/img/uploads`));
+		$petImg = $images[array_rand($images)];
+
+		$img = new Image();
+		$img->pet_id   = $pet->id;
+		$img->img_path = "/img/uploads/$petImg";
+		$img->save();
 
     	$this->info('pet created!');
 
