163ffe159fc1e17b5668039d263caa12071ab532
zgulde <zachgulde@gmail.com>
2016-01-29 10:08:01 -0600

    added contact us route

--- app/routes.php
+++ app/routes.php
@@ -91,3 +91,9 @@ Route::get('users/logout', 'UsersController@logout');
 Route::post('/images/pet', 'ImagesController@uploadPetImage');
 Route::post('/images/user', 'ImagesController@uploadUserImage');
 Route::delete('/images/{id}', 'ImagesController@destroy');
+
+// Contact
+Route::get('/contact', function()
+{
+    return View::make('contact');
+});
