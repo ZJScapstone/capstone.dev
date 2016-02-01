4613d8c753db407296233aed21b23d682128612a
Jonathan R <jreyes.satx@gmail.com>
2016-02-01 09:37:41 -0600

    modifed style for login (padding and button style), updated style for show user profile

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -1,34 +1,36 @@
 @extends ('layouts.master')
 <div class="container">
-    <form role="form" method="POST" action="{{{ URL::to('/users/login') }}}" accept-charset="UTF-8">
-        <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
-            <div class="form-group">
-                <label for="email">{{{ Lang::get('confide::confide.e_mail') }}}</label>
-                <input class="form-control" tabindex="1" placeholder="{{{ Lang::get('confide::confide.e_mail') }}}" type="text" name="email" id="email" value="{{{ Input::old('email') }}}">
-            </div>
-            <div class="form-group">
-            <label for="password">
-                {{{ Lang::get('confide::confide.password') }}}
-            </label>
-            <input class="form-control" tabindex="2" placeholder="{{{ Lang::get('confide::confide.password') }}}" type="password" name="password" id="password">
-            <p class="help-block">
-                <a href="{{{ URL::to('/users/forgot_password') }}}">{{{ Lang::get('confide::confide.login.forgot_password') }}}</a>
-            </p>
-            </div>
-            <div class="checkbox">
-                <label for="remember">
-                    <input tabindex="4" type="checkbox" name="remember" id="remember" value="1"> {{{ Lang::get('confide::confide.login.remember') }}}
+    <div class="section">
+        <form role="form" method="POST" action="{{{ URL::to('/users/login') }}}" accept-charset="UTF-8">
+            <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
+                <div class="form-group">
+                    <label for="email">{{{ Lang::get('confide::confide.e_mail') }}}</label>
+                    <input class="form-control" tabindex="1" placeholder="{{{ Lang::get('confide::confide.e_mail') }}}" type="text" name="email" id="email" value="{{{ Input::old('email') }}}">
+                </div>
+                <div class="form-group">
+                <label for="password">
+                    {{{ Lang::get('confide::confide.password') }}}
                 </label>
-            </div>
-            @if (Session::get('error'))
-                <div class="alert alert-error alert-danger">{{{ Session::get('error') }}}</div>
-            @endif
+                <input class="form-control" tabindex="2" placeholder="{{{ Lang::get('confide::confide.password') }}}" type="password" name="password" id="password">
+                <p class="help-block">
+                    <a href="{{{ URL::to('/users/forgot_password') }}}">{{{ Lang::get('confide::confide.login.forgot_password') }}}</a>
+                </p>
+                </div>
+                <div class="checkbox">
+                    <label for="remember">
+                        <input tabindex="4" type="checkbox" name="remember" id="remember" value="1"> {{{ Lang::get('confide::confide.login.remember') }}}
+                    </label>
+                </div>
+                @if (Session::get('error'))
+                    <div class="alert alert-error alert-danger">{{{ Session::get('error') }}}</div>
+                @endif
 
-            @if (Session::get('notice'))
-                <div class="alert">{{{ Session::get('notice') }}}</div>
-            @endif
-            <div class="form-group">
-                <button tabindex="3" type="submit" class="btn btn-default">{{{ Lang::get('confide::confide.login.submit') }}}</button>
-            </div>
-    </form>
+                @if (Session::get('notice'))
+                    <div class="alert">{{{ Session::get('notice') }}}</div>
+                @endif
+                <div class="form-group">
+                    <button tabindex="3" type="submit" class="btn btn-default">{{{ Lang::get('confide::confide.login.submit') }}}</button>
+                </div>
+        </form>
+    </div>
 </div>
