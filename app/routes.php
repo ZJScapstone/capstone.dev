9350157d2b57f3919f75399c78b1f26dd16bd9fd
zgulde <zachgulde@gmail.com>
2016-02-02 11:17:40 -0600

    fix for routes

--- app/routes.php
+++ app/routes.php
@@ -91,7 +91,6 @@ Route::post('users/forgot_password', 'UsersController@doForgotPassword');
 Route::get('users/reset_password/{token}', 'UsersController@resetPassword');
 Route::post('users/reset_password', 'UsersController@doResetPassword');
 Route::get('users/logout', 'UsersController@logout');
-Route::get('users/{id}', 'UsersController@show');
 Route::get('users/{idOrEmail}', 'UsersController@showProfile');
 
 // Image routes
